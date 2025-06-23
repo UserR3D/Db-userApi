a0522334e5f44ce9ab0b451b9e7147e78fd1e825
red <kauawar04@gmail.com>
2025-06-23 20:07:15 -0300

    typo correction

--- src/routes/postsRoute.ts
+++ src/routes/postsRoute.ts
@@ -1,9 +1,9 @@
-import { FastifyInstance } from 'fastify';
-import { getHandlerPosts, postCreate } from '../controllers/postsController';
-import { postDbSchema } from '../schemas/userSchema';
+import { FastifyInstance } from "fastify";
+import { getHandlerPosts, postCreate } from "../controllers/postsController";
+import { postDbSchema } from "../schemas/userSchema";
 async function postsRoute(route: FastifyInstance) {
-  route.post('/user/:id/post', { schema:  { body: postDbSchema }}, postCreate);
-  route.get('/user/posts', getHandlerPosts);
+  route.post("/users/:id/post", { schema: { body: postDbSchema } }, postCreate);
+  route.get("/users/posts", getHandlerPosts);
 }
 
 export default postsRoute;
